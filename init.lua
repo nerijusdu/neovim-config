2539996eee284c8355c3409527144329c5f617ce
Nerijus <dnerijus72@gmail.com>
2025-09-29 21:21:38 +0300

    tab to select suggestion

--- init.lua
+++ init.lua
@@ -3,6 +3,5 @@ require("config.dotenv")
 require("config.lazy")
 
 -- TODO:
--- Tab to select next suggestion in cmp
 -- eslint errors as warnings
 -- gF not working
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,12 +2,29 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = function(_, opts)
+			opts.servers = opts.servers or {}
+
 			if vim.env.PLUGIN_ESP32 == "true" then
 				local esp32 = require("esp32")
-				opts.servers = opts.servers or {}
 				opts.servers.clangd = esp32.lsp_config()
-				return opts
 			end
+
+			if opts.servers.eslint then
+				opts.servers.eslint.handlers = {
+					["textDocument/publishDiagnostics"] = function(_, result, ctx)
+						if result.diagnostics ~= nil then
+							for _, d in ipairs(result.diagnostics) do
+								if d.severity == vim.diagnostic.severity.ERROR then
+									d.severity = vim.diagnostic.severity.WARN
+								end
+							end
+						end
+						vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
+					end,
+				}
+			end
+
+			return opts
 		end,
 	},
 }
