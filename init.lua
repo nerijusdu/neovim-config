ece891d86780d9213762604679189fc396943f41
Nerijus <nerijus.dulke@mokausi.lt>
2025-09-29 19:51:03 +0300

    add todo list

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.dotenv")
 require("config.lazy")
+
+-- TODO:
+-- Tab to select next suggestion in cmp
+-- eslint errors as warnings
+-- gF not working
+-- toggle comments
+-- dont open file tree on startup
