dd0c264a1b465fc7a86f163a13157ed20136faf0
Nerijus <dnerijus72@gmail.com>
2026-05-21 23:09:42 +0300

    fix filetype not detected

--- init.lua
+++ init.lua
@@ -4,3 +4,11 @@ require("config.lazy")
 
 -- TODO:
 -- gF not working
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.schedule(function()
+			vim.cmd("silent! bufdo filetype detect")
+		end)
+	end,
+})
